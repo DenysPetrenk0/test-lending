e95aaa52e6e8cffb5509237772acadc13adf7838
DenysPetrenko <p.gesser2015@gmail.com>
2025-09-22 01:22:16 +0300

    #6 added markup and styles

--- front/js/main.js
+++ front/js/main.js
@@ -29,24 +29,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const ageStatus = localStorage.getItem('ageStatus');
 
     if (ageStatus === 'true') {
-        modal.style.display = 'none';
-        body.classList.remove('noScroll');
+        if (modal) modal.style.display = 'none';
+        body.style.overflow = 'auto';
     } else if (ageStatus === 'false') {
-        modal.style.display = 'none';
-        body.classList.remove('noScroll');
+        if (modal) modal.style.display = 'none';
+        body.style.overflow = 'auto';
         main.innerHTML = main.innerHTML = '<div class="no-access"><p class="no-access__text">Vous ne remplissez pas les critères d’âge requis pour accéder à ce site. L’accès est exclusivement réservé aux personnes majeures (18+). Merci de votre compréhension.</p></div>';
     }
+    if (modal) {
+        btnYes.addEventListener('click', () => {
+            modal.style.display = 'none';
+            body.style.overflow = 'auto';
+            localStorage.setItem('ageStatus', 'true');
+        });
 
-    btnYes.addEventListener('click', () => {
-        modal.style.display = 'none';
-        body.classList.remove('noScroll');
-        localStorage.setItem('ageStatus', 'true');
-    });
-
-    btnNo.addEventListener('click', () => {
-        modal.style.display = 'none';
-        main.innerHTML = '<div class="no-access"><p class="no-access__text">Vous ne remplissez pas les critères d’âge requis pour accéder à ce site. L’accès est exclusivement réservé aux personnes majeures (18+). Merci de votre compréhension.</p></div>';
-        body.classList.remove('noScroll');
-        localStorage.setItem('ageStatus', 'false');
-    });
+        btnNo.addEventListener('click', () => {
+            modal.style.display = 'none';
+            main.innerHTML = '<div class="no-access"><p class="no-access__text">Vous ne remplissez pas les critères d’âge requis pour accéder à ce site. L’accès est exclusivement réservé aux personnes majeures (18+). Merci de votre compréhension.</p></div>';
+            body.style.overflow = 'auto';
+            localStorage.setItem('ageStatus', 'false');
+        });
+    }
 });
